2f472512acda85df42a7bd18f6393f89f42eddb0
Mexidis <mexidismartinez@gmail.com>
2024-12-23 00:04:56 -0600

    reestructuración

---
+++ leetcode/easy/roman_to_integer/romans.cpp
@@ -0,0 +1,16 @@
+// Created by pac61 on 22/12/2024.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string romanToInt(string s) {
+
+}
+
+int main() {
+  string s;
+  cin >> s;
+
+  int ans = romanToInt(s);
+}
+
