e6dc9002a193c57ae536a223928915e8d5e3c20f
Patrick Martínez <mexidismartinez@gmail.com>
2025-01-26 00:42:48 -0600

    Empezado longest-common-prefix

---
+++ leetcode/easy/longest_common_prefix/lcp.cpp
@@ -0,0 +1,25 @@
+//
+// Created by pac61 on 25/01/2025.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+    string out = "";
+    for (int palabraActual = 0; palabraActual < strs.size(); palabraActual++) {
+      char caracter = ;
+      for (int caracterActual = 0; caracterActual < strs[palabraActual].length(); caracterActual++) {
+          caracter = strs[palabraActual].at(caracterActual);
+          continue;
+      }
+    }
+
+
+
+}
+
+int main() {
+    vector<string> words;
+    string s = ;
+}
